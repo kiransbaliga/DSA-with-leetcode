6be871f35014e6ca20a3158ba42a7f618844288c
Kiran S Baliga <60563196+kiransbaliga@users.noreply.github.com>
2021-07-21 19:30:01 +0530

    minor

--- py copy.py
+++ py copy.py
@@ -1,19 +1,27 @@
+# Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+# k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
+# You may not alter the values in the list's nodes, only nodes themselves may be changed.
 
-def solve():
-    
-    n=int(input())
-    lst=list(map(int,input().split()))
-    s=sum(lst)
-    if (s==n):
-        print("0")
-    elif(s<=0):
-        print("1")
-    elif (s>0):
-        print(s-n)
-        
-def main():
-    t=int(input())
-    while(t>=0):
-            solve() 
-            t-+1 
-main()
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+        if k >= 2:
+            cur=head
+            count=0
+            while cur and count<k:
+                cur=cur.next
+                count+=1
+            if count==k:
+                cur=self.reverseKGroup(cur,k)
+                while count>0:
+                    tmp=head.next
+                    head.next=cur
+                    cur=head
+                    head=tmp
+                    count-=1
+                head=cur
+        return head
