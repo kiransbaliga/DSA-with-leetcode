f0dcc1b8d4d05a8884fbcbe132addbeba7111059
Kiran S Baliga <kiransbaliga@gmail.com>
2021-08-27 20:58:02 +0530

    Leetcode Qstn 27 aug added

--- py.py
+++ py.py
@@ -1,17 +1,18 @@
-import itertools
-
-s='green'
-l1= list(itertools.combinations(s,1))
-l2= list(itertools.combinations(s,2))
-l3= list(itertools.combinations(s,3))
-l4= list(itertools.combinations(s,4))
-
-for i in l1:
-    print(*i)
-for i in l2:
-    print(*i)
-for i in l3:
-    print(*i)
-for i in l4:
-    print(*i)
-print("green")
+# class Solution:
+#     def findLUSlength(self, strs: List[str]) -> int:
+#         strs=list(sorted(strs,key=len))
+#         for i in range(len(strs)-1):
+#             for j in range(i+1,len(strs)):
+                
+class Solution:
+        def findLUSlength(self, words):
+            def isSubsequence(s, t):
+                t = iter(t)
+                return all(c in t for c in s)
+ 
+            words.sort(key = lambda x:-len(x))
+            for i, word in enumerate(words):
+                if all(not isSubsequence(word, words[j]) for j in range(len(words)) if j != i): 
+                    return len(word)
+        
+            return -1
