cc4c82959cc7dfbae319856b3c0cb22e9483d1d3
Kiran S Baliga <kiransbaliga@gmail.com>
2021-08-30 11:31:04 +0530

    29_AUG

--- py.py
+++ py.py
@@ -1,12 +1,14 @@
 class Solution:
-    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
-        
-        joball=sorted(list(zip(startTime, endTime, profit)))
-        st=[i[0]  for i in joball]
-        n=len(joball)
-        dp=[0] * (n+1)
-        for k in range(n-1,-1,-1):
-            temp=bisect_left(st,joball[k][1])
-            dp[k]=max(joball[k][2] + dp[temp], dp[k+1])
-        return dp[0]
-        
+    def minPatches(self, nums: List[int], n: int) -> int:
+        cnt=0 # cnt is the number of patches
+        summ=0 # summ is the total sum that can be reached from the elements in the array
+        i=0 # i is the index of the current element
+        m=len(nums)
+        while summ<n:  # if the total sum is less than n, then we need to add more elements to the array
+            if i >= m or nums[i] > summ+1: # if the current element is greater than the total sum, then we need to add more elements to the array
+                summ += summ+1 # add the current element to the total sum
+                cnt+=1 # add one patch
+            else:
+                summ += nums[i] # add the current element to the total sum
+                i+=1 # move to the next element
+        return cnt 
