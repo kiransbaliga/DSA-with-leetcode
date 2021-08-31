9949d3b92ff350b32406e196800d6475e4bbd81f
Kiran S Baliga <kiransbaliga@gmail.com>
2021-08-31 12:41:13 +0530

    Aug_30

--- py.py
+++ py.py
@@ -1,14 +1,10 @@
 class Solution:
-    def minPatches(self, nums: List[int], n: int) -> int:
-        cnt=0 # cnt is the number of patches
-        summ=0 # summ is the total sum that can be reached from the elements in the array
-        i=0 # i is the index of the current element
-        m=len(nums)
-        while summ<n:  # if the total sum is less than n, then we need to add more elements to the array
-            if i >= m or nums[i] > summ+1: # if the current element is greater than the total sum, then we need to add more elements to the array
-                summ += summ+1 # add the current element to the total sum
-                cnt+=1 # add one patch
-            else:
-                summ += nums[i] # add the current element to the total sum
-                i+=1 # move to the next element
-        return cnt 
+    def maxCount(self, m: int, n: int, ops: List[List[int]]) -> int:
+        # the logic is to find the min of the options cause thats the part which is incremented the most times. 
+        # We also have to check if ops contain anythong ass well.
+        if not ops:return m*n
+        a,b=m,n
+        for i,j in ops:
+            a=min(a,i)
+            b=min(b,j)
+        return a*b   
