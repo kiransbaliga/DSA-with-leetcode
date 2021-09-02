6dbd30614d76f0da1505a4dda37e23b8a380cd4b
Kiran S Baliga <kiransbaliga@gmail.com>
2021-09-02 14:23:48 +0530

    sept_2

--- py.py
+++ py.py
@@ -1,10 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def findMin(self, nums: List[int]) -> int:
-        left, right = 0, len(nums) - 1 # left and right pointer
-        while nums[left] > nums[right]: # if left > right, then the min is in the middle
-            middle  = (left + right) // 2 #
-            if nums[middle] < nums[right]: # if middle > right, then the min is in the right
-                right = middle
-            else:
-                left = middle + 1
-        return nums[left]
+    def generateTrees(self, n: int) -> List[Optional[TreeNode]]:
+        def formtree(start,end): 
+            if start==end: return None   # to check if the tree is empty
+            res=[]                       
+            for i in range(start,end):      # loop each number 
+                for l in formtree(start,i) or [None]:  # form to the left of the tree i.e. for numbers less than the selected number
+                    for r in formtree(i+1,end) or [None]: # form to the right of the tree i.e. for numbers greater than the selected number
+                        node= TreeNode(i)   # create a node
+                        node.left=l     
+                        node.right=r
+                        res.append(node)
+            return res
+        return formtree(1,n+1)
+        
