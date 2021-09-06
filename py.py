d641fbb6f253d530f443c72ea541dc4f8949fff3
Kiran S Baliga <kiransbaliga@gmail.com>
2021-09-06 20:11:33 +0530

    Sept 6 updated

--- py.py
+++ py.py
@@ -1,34 +1,13 @@
-def median(nums,k):
-    nums.sort()
-    if k%2==0:
-        return(nums[(k//2)-1])
-    else:
-        return(nums[k//2])        
+class Solution:
+    def slowestKey(self, releaseTimes: List[int], keysPressed: str) -> str:
+        finkey=keysPressed[0]
+        fintime=releaseTimes[0]
+        for i in range(1,len(releaseTimes)):
+            reltime=releaseTimes[i]-releaseTimes[i-1]
+            key=keysPressed[i]
 
-def subseq(arr,idx,subaarr,lst,k):
-    
-    if idx == len(arr):
-        if len(subaarr)==k:
-            lst.append(subaarr)
-    else:
-        subseq(arr,idx+1,subaarr,lst,k)
-        subseq(arr,idx+1,subaarr+[arr[idx]],lst,k)
-    
-t=int(input())
-for _ in range(t):
-    lst=[]
-    ans=[]
-    res=-1
-    n,k=map(int,input().split())
-    a=list(map(int,input().split()))
-    subseq(a,0,[],lst,k)
-    for i in lst:
-        cpy=i
-        tmp=median(i,k)
-        if tmp>res:
-            res=tmp
-            ans=cpy
-    print(res)
-    print(*ans)
+            if reltime>fintime or (reltime==fintime and key>finkey):
+                fintime=reltime 
+                finkey=key  
+        return finkey
 
-                
