8a1163f66736f65a5bb692a4c37922180f02db1e
Kiran S Baliga <kiransbaliga@gmail.com>
2021-09-09 20:24:33 +0530

    sept9

--- py.py
+++ py.py
@@ -1,33 +1,34 @@
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
 class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        newhead= ListNode()
-        def linkofthelink(orig,rev):
-            stack=[]
-            temp=orig
-            while temp:
-                stack.append(temp)
-                temp=temp.next
-            
-            temp=stack.pop()
-            rev=temp
-            
-            while stack:
-                i=stack.pop()  
-                temp.next=i    
-                temp= temp.next
-            
-            temp.next = None     
-            return rev
+    def orderOfLargestPlusSign(self, n: int, mines: List[List[int]]) -> int:
+        m=len(mines)
 
-        if head:
-            newhead = linkofthelink(head,newhead)
-            return newhead
-        else: 
-            return None
+        if m==n*n: return 0
+        
+        vis=set()
+        
+        for i,j in mines:
+            vis.add((i,j))   # mark all Zeroes
+        ans=1
 
-            
+        # left, right , up , down...
+        lurd=[[[0,0,0,0] for _ in range(n)] for _ in range(n)]
+        
+        for i in range(n):
+            for j in range(n):
+
+                if (i,j) not in vis:
+                     # check left and up from 0-n
+        
+                    lurd[i][j][0]=(lurd[i][j-1][0]+1) if j-1>=0 else 1   
+                    lurd[i][j][1]=(lurd[i-1][j][1]+1) if i-1>=0 else 1
+        
+        for i in range(n-1,-1,-1):
+            for j in range(n-1,-1,-1):
+
+                if (i,j) not in vis:
+                         # check right and down from n-1 to 0 
+                    lurd[i][j][2]=(lurd[i][j+1][2]+1) if j+1<n else 1
+                    lurd[i][j][3]=(lurd[i+1][j][3]+1) if i+1<n else 1
+
+                ans=max(ans,min(lurd[i][j]))
+        return ans
