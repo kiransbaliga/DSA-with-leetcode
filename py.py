329538c934ba3ca17d3ed1a314be3c4a13f023b7
kiransbaliga <kiransbaliga@gmail.com>
2021-09-11 22:32:55 +0530

    sept11

--- py.py
+++ py.py
@@ -1,34 +1,28 @@
 class Solution:
-    def orderOfLargestPlusSign(self, n: int, mines: List[List[int]]) -> int:
-        m=len(mines)
-
-        if m==n*n: return 0
+    def calculate(self, s):    
+        def calc(tmp):
+            def update(op, v):
+                if op == "+": stack.append(v)
+                if op == "-": stack.append(-v)
+                if op == "*": stack.append(stack.pop() * v)
+                if op == "/": stack.append(int(stack.pop() / v))
         
-        vis=set()
-        
-        for i,j in mines:
-            vis.add((i,j))   # mark all Zeroes
-        ans=1
+            num, stack, sign = 0, [], "+"
+            
+            while tmp < len(s):
+                if s[tmp].isdigit():
+                    num = num * 10 + int(s[tmp])
+                elif s[tmp] in "+-*/":
+                    update(sign, num)
+                    num, sign = 0, s[tmp]
+                elif s[tmp] == "(":
+                    num, j = calc(tmp + 1)
+                    tmp = j - 1
+                elif s[tmp] == ")":
+                    update(sign, num)
+                    return sum(stack), tmp + 1
+                tmp += 1
+            update(sign, num)
+            return sum(stack)
 
-        # left, right , up , down...
-        lurd=[[[0,0,0,0] for _ in range(n)] for _ in range(n)]
-        
-        for i in range(n):
-            for j in range(n):
-
-                if (i,j) not in vis:
-                     # check left and up from 0-n
-        
-                    lurd[i][j][0]=(lurd[i][j-1][0]+1) if j-1>=0 else 1   
-                    lurd[i][j][1]=(lurd[i-1][j][1]+1) if i-1>=0 else 1
-        
-        for i in range(n-1,-1,-1):
-            for j in range(n-1,-1,-1):
-
-                if (i,j) not in vis:
-                         # check right and down from n-1 to 0 
-                    lurd[i][j][2]=(lurd[i][j+1][2]+1) if j+1<n else 1
-                    lurd[i][j][3]=(lurd[i+1][j][3]+1) if i+1<n else 1
-
-                ans=max(ans,min(lurd[i][j]))
-        return ans
+        return calc(0)
