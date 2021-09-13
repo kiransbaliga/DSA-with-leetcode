a97dfaac06b095b965945588f9756d6830045a45
kiransbaliga <kiransbaliga@gmail.com>
2021-09-13 20:41:13 +0530

    sept 13

--- py.py
+++ py.py
@@ -1,28 +1,12 @@
 class Solution:
-    def calculate(self, s):    
-        def calc(tmp):
-            def update(op, v):
-                if op == "+": stack.append(v)
-                if op == "-": stack.append(-v)
-                if op == "*": stack.append(stack.pop() * v)
-                if op == "/": stack.append(int(stack.pop() / v))
+    def maxNumberOfBalloons(self, text: str) -> int:
+        b=text.count('b')
+        a=text.count('a')
+        l=text.count('l')
+        o=text.count('o')
+        n=text.count('n')
+        l=l//2
+        o=o//2  
+        y=min(b,a,l,o,n)
+        return y
         
-            num, stack, sign = 0, [], "+"
-            
-            while tmp < len(s):
-                if s[tmp].isdigit():
-                    num = num * 10 + int(s[tmp])
-                elif s[tmp] in "+-*/":
-                    update(sign, num)
-                    num, sign = 0, s[tmp]
-                elif s[tmp] == "(":
-                    num, j = calc(tmp + 1)
-                    tmp = j - 1
-                elif s[tmp] == ")":
-                    update(sign, num)
-                    return sum(stack), tmp + 1
-                tmp += 1
-            update(sign, num)
-            return sum(stack)
-
-        return calc(0)
