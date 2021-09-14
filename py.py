3a601c6a4d3fb1314b0afe3e06513770edceaa83
kiransbaliga <kiransbaliga@gmail.com>
2021-09-14 14:30:30 +0530

    sept 14

--- py.py
+++ py.py
@@ -1,12 +1,15 @@
 class Solution:
-    def maxNumberOfBalloons(self, text: str) -> int:
-        b=text.count('b')
-        a=text.count('a')
-        l=text.count('l')
-        o=text.count('o')
-        n=text.count('n')
-        l=l//2
-        o=o//2  
-        y=min(b,a,l,o,n)
-        return y
-        
+    def reverseOnlyLetters(self, s: str) -> str:
+        n=len(s)
+        i=0
+        j=n-1
+        while i<j:
+            while i<n and not s[i].isalpha():
+                i+=1
+            while j>=0 and not s[j].isalpha():
+                j-=1
+            if i<j:
+                s=s[:i]+s[j]+s[i+1:j]+s[i]+s[j+1:]
+            i+=1
+            j-=1
+        return s
