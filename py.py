7950b984606ec76f4a9b75b35b75f8a3761a3046
kiransbaliga <kiransbaliga@gmail.com>
2021-09-15 16:43:14 +0530

    sept_15

--- py.py
+++ py.py
@@ -1,15 +1,22 @@
 class Solution:
-    def reverseOnlyLetters(self, s: str) -> str:
-        n=len(s)
-        i=0
-        j=n-1
-        while i<j:
-            while i<n and not s[i].isalpha():
-                i+=1
-            while j>=0 and not s[j].isalpha():
-                j-=1
-            if i<j:
-                s=s[:i]+s[j]+s[i+1:j]+s[i]+s[j+1:]
-            i+=1
-            j-=1
-        return s
+    def maxTurbulenceSize(self, arr: List[int]) -> int:
+        if len(arr) < 2:
+            return len(arr)
+        res = 1
+        cur = 1
+        for i in range(1, len(arr)-1):
+            if cur>1:
+                if  (gt and arr[i] > arr[i+1]) or ((not gt) and arr[i] < arr[i+1]):
+                    cur += 1
+                    gt=not gt
+                elif arr[i]==arr[i+1]:
+                    res=max(res, cur)
+                    cur=1
+                else:
+                    res=max(res, cur)
+                    cur=2
+                    gt=arr[i]<arr[i+1]
+            elif arr[i]!=arr[i-1]:
+                cur=2
+                gt=arr[i]<arr[i+1]
+        return max(res, cur)
