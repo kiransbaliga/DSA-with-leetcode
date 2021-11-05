5a710fca5e91bea706000d7ed61174462f994888
Kiran <kiransbaliga@gmail.com>
2021-11-05 23:01:24 +0530

    nov 3 and 4

--- py.py
+++ py.py
@@ -1,21 +1,9 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        def traversal(root,i):
-            if root:
-                if root.left == None and root.right == None:
-                    return i*10+root.val
-                else:
-                    return traversal(root.left,i*10+root.val)+traversal(root.right,i*10+root.val)
-            else:
-                return 0
-        if root:
-            return traversal(root,0)
-        else:
-            return 0
-        
+    def arrangeCoins(self, n: int) -> int:
+        lvl=0
+        i=1
+        while n>=0:
+            n=n-i
+            i+=1
+            lvl+=1
+        return lvl-1
