409fffe1cb041e218a37c03b4765aacc0bb30474
Jithin A P <53345340+Jithin-A-P@users.noreply.github.com>
2022-10-24 20:53:55 +0530

    Added factorial using recursion

--- random_python_code.py
+++ random_python_code.py
@@ -14,3 +14,19 @@ y = temp
 
 print('The value of x after swapping: {}'.format(x))
 print('The value of y after swapping: {}'.format(y))
+
+
+# New temporary code
+def fact_rec(n):
+	if n < 0:
+		return
+	elif n <= 1:
+		return 1
+	else:
+		return n*fact_rec(n-1)
+
+answer = fact_rec(4)
+if answer == None: 
+	print("Cannot calculate factorial of a negative value")
+else:
+	print(answer)  # 24 = 4x3x2x1 = 4! 
